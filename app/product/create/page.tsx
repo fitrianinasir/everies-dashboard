a2099fb760e9c183d74a7389eaee61764489c862
Fitriani Nasir <fitrianinasir8@gmail.com>
2024-02-04 14:45:00 +0700

    stock handler done

--- app/product/create/page.tsx
+++ app/product/create/page.tsx
@@ -14,13 +14,18 @@ import {
 import { ReactSortable } from "react-sortablejs";
 import axios from "axios";
 
-type Category = {
+type RawCategory = {
   id: number;
   title: string;
   type: string;
 };
 
-type SUBCATEGORIES = {
+type Category = {
+  id: number;
+  name: string;
+};
+
+type ProductType = {
   id: number;
   category_id: number;
   title: string;
@@ -33,41 +38,104 @@ type STOCK_BY_SIZE = {
   amount: number;
 };
 
+type PRODUCT = {
+  title: string;
+  category_id: number;
+  type_id: number;
+  price: number;
+  stock_by_size: STOCK_BY_SIZE_RESULT[];
+  stock_by_type: STOCK_BY_TYPE_RESULT[];
+  img: string;
+  rate: number;
+  sold: number;
+};
+
+interface STOCK_BY_SIZE_RESULT {
+  [size: string]: {
+    [color: string]: number;
+  };
+}
+
+interface STOCK_BY_TYPE_RESULT {
+  [color: string]: {
+    [size: string]: number;
+  };
+}
+
 const AddProduct = () => {
+  const initialStockBySize: STOCK_BY_SIZE[] = [
+    { id: 1, size: "", color: "", amount: 0 },
+  ];
+
+  const initialFields = {
+    title: "",
+    category_id: 0,
+    type_id: 0,
+    price: 0,
+    stock_by_size: [],
+    stock_by_type: [],
+    img: "",
+    rate: 0,
+    sold: 0,
+  };
+
   const [categories, setCategories] = useState<Category[]>([]);
-  const [subcategories, setSubCategories] = useState<SUBCATEGORIES[]>([]);
-  const [stockBySize, setStockBySize] = useState<STOCK_BY_SIZE[]>([
-    { id: 1, size: "S", color: "", amount: 0 },
-    { id: 2, size: "M", color: "", amount: 0 },
-    { id: 3, size: "L", color: "", amount: 0 },
-    { id: 4, size: "XL", color: "", amount: 0 },
-  ]);
+  const [productType, setProductTypes] = useState<ProductType[]>([]);
+  const [stockBySize, setStockBySize] =
+    useState<STOCK_BY_SIZE[]>(initialStockBySize);
+
+  const [product, setProduct] = useState<PRODUCT>(initialFields);
 
   useEffect(() => {
     getCategoryData();
-    getSubCategories();
+    getProductTypes();
   }, []);
-
-  useEffect(() => {
-    console.log(stockBySize);
-  }, [stockBySize]);
 
   const getCategoryData = async () => {
     await axios
       .get(`${BASE_URL}/categories`)
       .then((res) => {
-        setCategories(res.data.data);
+        let modifdata: Category[] = [];
+        let rawdata: RawCategory[] = res.data.data;
+        rawdata.map((i): any => {
+          let tmp = {
+            id: i.id,
+            name: `${i.title} ${i.type}`,
+          };
+          modifdata.push(tmp);
+        });
+        setCategories(modifdata);
       })
       .catch((err) => console.log(err));
   };
 
-  const getSubCategories = async () => {
+  const getProductTypes = async () => {
     await axios
-      .get(`${BASE_URL}/sub-categories`)
+      .get(`${BASE_URL}/product-types`)
       .then((res) => {
-        setSubCategories(res.data.data);
+        setProductTypes(res.data.data);
       })
       .catch((err) => console.log(err));
+  };
+
+  // ====== HANDLER ======
+
+  const resetFields = () => setProduct(initialFields);
+
+  const selectCategoryHandler = (e: any) => {
+    const selectedID = parseInt(e.target.value);
+    setProduct((prev) => ({
+      ...prev,
+      category_id: selectedID,
+    }));
+  };
+
+  const selectProductTypeHandler = (e: any) => {
+    const selectedID = parseInt(e.target.value);
+    setProduct((prev) => ({
+      ...prev,
+      type_id: selectedID,
+    }));
   };
 
   const addStockBySizeHandler = () => {
@@ -110,7 +178,57 @@ const AddProduct = () => {
     setStockBySize(dataTmp);
   };
 
-  
+  const modifStockBySizeAndType = () => {
+    let stockBySizeResult: STOCK_BY_SIZE_RESULT[] = [];
+    let stockByTypeResult: STOCK_BY_TYPE_RESULT[] = [];
+
+    stockBySize.map((item): any => {
+      const { size, color, amount } = item;
+      let tmpSize: Record<string, Record<string, number>> = {};
+      let tmpType: Record<string, Record<string, number>> = {};
+
+      tmpSize[size] = { [color]: amount };
+      tmpType[color] = { [size]: amount };
+
+      const existingSizeIdx = stockBySizeResult.findIndex((existingItem) =>
+        existingItem.hasOwnProperty(size)
+      );
+
+      const existingTypeIdx = stockByTypeResult.findIndex((existingItem) =>
+      existingItem.hasOwnProperty(color)
+    );
+
+      if (existingSizeIdx !== -1) {
+        // Update existing entry by adding new color and amount
+        stockBySizeResult[existingSizeIdx][size][color] = amount;
+      } else {
+        // Add tmpSize to stockBySizeResult if size doesn't exist
+        tmpSize[size] = { [color]: amount };
+        stockBySizeResult.push(tmpSize);
+      }
+
+      if (existingTypeIdx !== -1) {
+        // Update existing entry by adding new size and amount
+        stockByTypeResult[existingTypeIdx][color][size] = amount;
+      } else {
+        // Add tmpType to stockByTypeResult if size doesn't exist
+        tmpType[color] = { [size]: amount };
+        stockByTypeResult.push(tmpType);
+      }
+    });    
+
+    setProduct((prev) => ({
+      ...prev,
+      stock_by_size: stockBySizeResult,
+      stock_by_type: stockByTypeResult
+    }))
+  };
+
+  const submitProduct = () => {
+    modifStockBySizeAndType();
+
+    console.log(product)
+  };
 
   return (
     <NextUIProvider>
@@ -124,29 +242,63 @@ const AddProduct = () => {
           </div>
           <div className="w-full h-[92%] p-5">
             <div className="w-full p-5 bg-white">
-              <div className="flex justify-between">
-                <h1 className="text-sm font-bold">Add Product</h1>
-              </div>
+              <h1 className="text-xl font-bold mb-5">ADD PRODUCT</h1>
               <div className="flex flex-col gap-4">
                 <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                  <Input type="text" label="Title" />
-                  <Select label="Select Category" className="max-w-xs">
-                    {categories.map((cat: Category) => (
-                      <SelectItem key={cat.id} value={cat.id}>
-                        {`${cat.title} ${cat.type}`}
+                  <Input
+                    type="text"
+                    label="Title"
+                    value={product.title}
+                    onChange={(e) =>
+                      setProduct((prev) => ({
+                        ...prev,
+                        title: e.target.value,
+                      }))
+                    }
+                  />
+                  <Input
+                    type="number"
+                    label="Price"
+                    value={product.price == 0 ? "" : product.price.toString()}
+                    className="w-1/6"
+                    onChange={(e) =>
+                      setProduct((prev) => ({
+                        ...prev,
+                        price: parseInt(e.target.value),
+                      }))
+                    }
+                  />
+                  <Select
+                    label="Category"
+                    className="w-2/6"
+                    selectedKeys={[product.category_id.toString()]}
+                    onChange={(e) => selectCategoryHandler(e)}
+                  >
+                    {categories.map((category: Category) => (
+                      <SelectItem
+                        key={category.id}
+                        value={category.name}
+                        textValue={category.name}
+                      >
+                        {category.name}
                       </SelectItem>
                     ))}
                   </Select>
-                  <Select label="Select Type Product" className="max-w-xs">
-                    {subcategories.map((sub: SUBCATEGORIES) => (
-                      <SelectItem key={sub.id} value={sub.title}>
-                        {sub.title}
+                  <Select
+                    label="Product Type"
+                    className="max-w-xs"
+                    selectedKeys={[product.type_id.toString()]}
+                    onChange={(e) => selectProductTypeHandler(e)}
+                  >
+                    {productType.map((type: ProductType) => (
+                      <SelectItem key={type.id} value={type.title}>
+                        {type.title}
                       </SelectItem>
                     ))}
                   </Select>
                 </div>
                 <div className="flex w-full justify-between  mt-5">
-                  <h3 className="text-md font-bold">Stock By Size</h3>
+                  <h3 className="text-xl font-bold">STOCK</h3>
 
                   <Button
                     color="primary"
@@ -160,53 +312,58 @@ const AddProduct = () => {
                   {stockBySize.map((item, idx) => (
                     <div
                       key={item.id}
-                      className="flex justify-between items-center mt-5 "
+                      className="flex flex-wrap md:flex-nowrap gap-10 text-xs cursor-grabbing mt-5 "
                     >
-                      <div className="flex w-[60%] flex-wrap md:flex-nowrap gap-10 text-xs cursor-grabbing">
-                        <Input
-                          type="text"
-                          variant="underlined"
-                          label="Size"
-                          value={item.size}
-                          onChange={(e) =>
-                            sizeChangeHandler(idx, e.target.value)
-                          }
-                        />
-                        <Input
-                          type="text"
-                          variant="underlined"
-                          label="Color"
-                          value={item.color}
-                          onChange={(e) =>
-                            colorChangeHandler(idx, e.target.value)
-                          }
-                        />
-                        <Input
-                          type="number"
-                          variant="underlined"
-                          label="Amount"
-                          value={String(item.amount)}
-                          onChange={(e) =>
-                            amountChangeHandler(idx, parseInt(e.target.value))
-                          }
-                        />
-                      </div>
-                      <Button
-                        color="danger"
-                        variant="solid"
-                        className="min-w-1"
-                        onClick={() => rmvStockBySizeHandler(idx)}
-                      >
-                        <img
-                          width="10"
-                          height="10"
-                          src="https://img.icons8.com/ios-filled/10/delete-sign--v1.png"
-                          alt="delete-sign--v1"
-                        />
-                      </Button>
+                      <Input
+                        type="text"
+                        variant="underlined"
+                        label="Size"
+                        value={item.size}
+                        onChange={(e) => sizeChangeHandler(idx, e.target.value)}
+                      />
+                      <Input
+                        type="text"
+                        variant="underlined"
+                        label="Color"
+                        value={item.color}
+                        onChange={(e) =>
+                          colorChangeHandler(idx, e.target.value)
+                        }
+                      />
+                      <Input
+                        type="number"
+                        variant="underlined"
+                        label="Amount"
+                        value={String(item.amount)}
+                        onChange={(e) =>
+                          amountChangeHandler(idx, parseInt(e.target.value))
+                        }
+                      />
+                      <button onClick={() => rmvStockBySizeHandler(idx)}>
+                        <img src="/icons/remove.png" alt="delete-sign--v1" />
+                      </button>
                     </div>
                   ))}
                 </ReactSortable>
+                <div>
+                  <h1 className="text-xl font-bold mt-5">IMAGES OF PRODUCT</h1>
+                </div>
+              </div>
+              <div className="flex justify-end gap-1 mt-5">
+                <Button
+                  variant="solid"
+                  color="warning"
+                  onClick={() => resetFields()}
+                >
+                  RESET
+                </Button>
+                <Button
+                  variant="solid"
+                  color="primary"
+                  onClick={() => submitProduct()}
+                >
+                  SUBMIT
+                </Button>
               </div>
             </div>
           </div>
